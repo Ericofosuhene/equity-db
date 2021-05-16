5a25572b59f3cd43adbd076fa2b05c234408225d
Alexd14 <32649372+Alexd14@users.noreply.github.com>
2021-05-16 13:41:07 -0400

    moved dispacher

--- equity_db/__init__.py
+++ equity_db/__init__.py
@@ -2,12 +2,10 @@ from . import variables
 from . import api
 from . import query
 from . import write
-from . import dispatcher
 
 __all__ = [
     'variables',
     'api',
     'query',
     'write',
-    'dispatcher'
 ]
