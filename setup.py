64d53d6f1441aaafaa389079569e120141ac30b2
Alexd14 <32649372+Alexd14@users.noreply.github.com>
2021-05-15 21:41:28 -0400

    pypi stuff

---
+++ setup.py
@@ -0,0 +1,31 @@
+from distutils.core import setup
+
+setup(
+    name='equity_db',
+    packages=['equity_db'],
+    version='1.0',
+    license='MIT',
+    description='Python package which manages CRSP & Compustat data. Uses MongoDB to store and query data.',
+    author='Alex DiCarlo',
+    author_email='dicarlo.a@northeastern.edu',
+    url='https://github.com/Alexd14/equity-db',
+    download_url='https://github.com/Alexd14/equity-db/archive/refs/tags/v1.0.tar.gz',
+    keywords=['CRSP', 'Compustat', 'stock database'],
+    install_requires=[
+        'pandas',
+        'numpy',
+        'pymongo',
+        'pandas_market_calendars',
+        'tqdm',
+    ],
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'Topic :: Software Development :: Build Tools',
+        'License :: OSI Approved :: MIT License',
+        'Programming Language :: Python :: 3.6',
+        'Programming Language :: Python :: 3.7',
+        'Programming Language :: Python :: 3.8',
+        'Programming Language :: Python :: 3.9',
+    ],
+)
